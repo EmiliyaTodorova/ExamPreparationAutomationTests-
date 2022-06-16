d918876f342465d13766cd3e46a40ac464b95b8c
Emiliya Todorova <104060345+EmiliyaTodorova@users.noreply.github.com>
2022-06-16 12:08:59 +0300

    Add files via upload

---
+++ Contacts.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json.Serialization;
+
+namespace APITests
+{
+    internal class Contacts
+    {
+        [JsonPropertyName("id")]
+        public int id { get; set; }
+
+        [JsonPropertyName("firstName")]
+        public int firstName { get; set; }
+
+        [JsonPropertyName("lastName")]
+        public int lastName { get; set; }
+
+        [JsonPropertyName("email")]
+        public int email { get; set; }
+
+        [JsonPropertyName("phone")]
+        public int phone { get; set; }
+
+        [JsonPropertyName("dateCreate")]
+        public int dateCreate { get; set; }
+
+        [JsonPropertyName("comments")]
+        public int comments { get; set; }
+
+    }
+}
